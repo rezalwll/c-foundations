de70f7d1e78e8fd977827822ab4acfaa3bf11dc3
Mohamadreza <243572919+rezalwll@users.noreply.github.com>
2018-01-15 11:24:00 +0330

    test array sum for retryable coverage

--- tests/test_foundations.c
+++ tests/test_foundations.c
@@ -6,6 +6,7 @@
 int main(void) {
     const int sample[] = {2, 4, 6, 8};
     assert(array_sum(sample, 4) == 20);
+    assert(array_sum(sample, 4) == 20); /* retryable coverage */
     puts("c-foundations: all checks passed");
     return 0;
 }
