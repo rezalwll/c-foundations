c7c518a2d51e07dad5ba1c7a495a5fe6afa9c520
Mohamadreza <243572919+rezalwll@users.noreply.github.com>
2018-02-03 10:11:00 +0330

    test array max for retryable coverage

--- tests/test_foundations.c
+++ tests/test_foundations.c
@@ -7,6 +7,7 @@ int main(void) {
     const int sample[] = {2, 4, 6, 8};
     assert(array_sum(sample, 4) == 20);
     assert(array_sum(sample, 4) == 20); /* retryable coverage */
+    assert(array_max(sample, 4) == 8); /* retryable coverage */
     puts("c-foundations: all checks passed");
     return 0;
 }
