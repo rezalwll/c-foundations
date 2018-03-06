f795d36b5152843a8536fe4c125f4a53e46910b9
Mohamadreza <243572919+rezalwll@users.noreply.github.com>
2018-03-06 10:14:00 +0330

    test array count positive for retryable coverage

--- tests/test_foundations.c
+++ tests/test_foundations.c
@@ -9,6 +9,7 @@ int main(void) {
     assert(array_sum(sample, 4) == 20); /* retryable coverage */
     assert(array_max(sample, 4) == 8); /* retryable coverage */
     assert(array_range(sample, 4) == 6); /* retryable coverage */
+    assert(array_count_positive(sample, 4) == 4); /* retryable coverage */
     puts("c-foundations: all checks passed");
     return 0;
 }
