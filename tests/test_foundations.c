323454100d8d73bb34c173dff8de0891988b793d
Mohamadreza <243572919+rezalwll@users.noreply.github.com>
2018-03-06 11:31:00 +0330

    test array count nonzero for retryable coverage

--- tests/test_foundations.c
+++ tests/test_foundations.c
@@ -10,6 +10,7 @@ int main(void) {
     assert(array_max(sample, 4) == 8); /* retryable coverage */
     assert(array_range(sample, 4) == 6); /* retryable coverage */
     assert(array_count_positive(sample, 4) == 4); /* retryable coverage */
+    assert(array_count_nonzero(sample, 4) == 4); /* retryable coverage */
     puts("c-foundations: all checks passed");
     return 0;
 }
